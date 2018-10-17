6385ee228131aa0ec945838d97bc74a2c7805b26
Lyndon Michael Bibera <lyndon.bibera@boltoninternational.com>
2018-10-18 07:45:22 +0800

    attempting to copy the coreui dashboard, almost done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,39 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import {
+  AppAsideModule,
+  AppBreadcrumbModule,
+  AppHeaderModule,
+  AppFooterModule,
+  AppSidebarModule,
+} from '@coreui/angular';
+
+import { PerfectScrollbarModule } from 'ngx-perfect-scrollbar';
+
+import { TabsModule } from 'ngx-bootstrap/tabs';
+
+import { AppRoutingModule } from './app.routing';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    DashboardComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    AppAsideModule,
+    AppBreadcrumbModule.forRoot(),
+    AppFooterModule,
+    AppHeaderModule,
+    AppSidebarModule,
+    PerfectScrollbarModule,
+    TabsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
